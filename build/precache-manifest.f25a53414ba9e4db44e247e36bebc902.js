a64d64e0d9c28e0f561411ca173bf9da56f175e9
Chandravardhan <45188575+Chandravardhan8998@users.noreply.github.com>
2020-06-22 18:23:34 +0530

    build commit

---
+++ build/precache-manifest.f25a53414ba9e4db44e247e36bebc902.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba52aa65896f9f003d227f7ef2202278",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c2166ab9c0e7f72a39fc",
+    "url": "/static/css/2.cbeaa07e.chunk.css"
+  },
+  {
+    "revision": "7b32686ee29b2c232a66",
+    "url": "/static/css/main.1ebc0a3e.chunk.css"
+  },
+  {
+    "revision": "c2166ab9c0e7f72a39fc",
+    "url": "/static/js/2.850ae083.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.850ae083.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b32686ee29b2c232a66",
+    "url": "/static/js/main.f48e332e.chunk.js"
+  },
+  {
+    "revision": "9816ff10e81ae0968cce",
+    "url": "/static/js/runtime-main.95ce22d6.js"
+  }
+]);
